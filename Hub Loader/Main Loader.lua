f42c8916eef53e37cab2600fa62b048ac4871732
Cips <135729464+CipsHub@users.noreply.github.com>
2023-06-19 17:42:02 +0300

    Update Main Loader.lua

--- Hub Loader/Main Loader.lua
+++ Hub Loader/Main Loader.lua
@@ -1,1 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Hub%20Loader/Loadstring.lua"))()
 
+-- my discord: c1ps dm me or create a ticket if u have issues (errors) with script
