dbdce16d181a61b43696d916a00f008a94502f30
Cips <135729464+CipsHub@users.noreply.github.com>
2023-06-19 17:40:50 +0300

    Update Loadstring.lua

--- Hub Loader/Loadstring.lua
+++ Hub Loader/Loadstring.lua
@@ -1,1 +1,4 @@
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Hub%20Loader/Blacklist%20Check.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Hub%20Loader/Whitelist%20Check.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Hub%20Loader/Premium%20Check.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Hub%20Loader/Game%20Checker%20-%20Script%20Loader.lua"))()
