8229b74551e72c972d3a33015509992dd0d66897
Cips <135729464+CipsHub@users.noreply.github.com>
2023-06-19 17:43:14 +0300

    Update Premium Script.lua

--- Hub Loader/Premium Script.lua
+++ Hub Loader/Premium Script.lua
@@ -1,2 +1,2 @@
 print("i still didnt make premium special script lol")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Hub%20Loader/Game%20Checker%20-%20Script%20Loader", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Hub%20Loader/Game%20Checker%20-%20Script%20Loader.lua", true))()
