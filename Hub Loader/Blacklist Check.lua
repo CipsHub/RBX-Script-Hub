d25d51b5be6b39f4a12d4ede26c228c95c56acf0
Cips <135729464+CipsHub@users.noreply.github.com>
2023-06-28 22:48:34 +0300

    Update Blacklist Check.lua

--- Hub Loader/Blacklist Check.lua
+++ Hub Loader/Blacklist Check.lua
@@ -5,51 +5,76 @@ local blacklist = {
 }
 
 local function isPlayerBlacklisted(player)
-    local playerId = player.UserId
-    for _, id in ipairs(blacklist) do
-        if playerId == id then
-            return true
-        end
-    end
-    return false
+	local playerId = player.UserId
+	for _, id in ipairs(blacklist) do
+		if playerId == id then
+			return true
+		end
+	end
+	return false
 end
 
 local function kickPlayer(player)
-local Decalid = 13893459024
+	local Decalid = 13893459024
 
-local DecalIDs = {
-	Top = Decalid,
-	Bottom = Decalid,
-	Front = Decalid,
-	Back = Decalid,
-	Left = Decalid,
-	Right = Decalid
-}
+	local DecalIDs = {
+		Top = Decalid,
+		Bottom = Decalid,
+		Front = Decalid,
+		Back = Decalid,
+		Left = Decalid,
+		Right = Decalid
+	}
 
-local function addDecal(part, face, decalID)
-	local decal = Instance.new("Decal")
-	decal.Texture = "rbxassetid://" .. decalID
-	decal.Face = face
-	decal.Parent = part
-end
-
-local function addDecalsToPart(part)
-	for face, decalID in pairs(DecalIDs) do
-		addDecal(part, Enum.NormalId[face], decalID)
+	local function addDecal(part, face, decalID)
+		local decal = Instance.new("Decal")
+		decal.Texture = "rbxassetid://" .. decalID
+		decal.Face = face
+		decal.Parent = part
 	end
-end
 
-for _, part in ipairs(workspace:GetDescendants()) do
-	if part:IsA("BasePart") then
-		addDecalsToPart(part)
-	end		
-end
-    wait(3)
-    player:Kick("🤡🤡You are banned from c1ps Hub🤡🤡")
+	local function addDecalsToPart(part)
+		for face, decalID in pairs(DecalIDs) do
+			addDecal(part, Enum.NormalId[face], decalID)
+		end
+	end
+
+	for _, part in ipairs(workspace:GetDescendants()) do
+		if part:IsA("BasePart") then
+			addDecalsToPart(part)
+		end		
+	end
+	wait(3)
+	player:Kick("You are banned from c1ps Hub reason: Being in Faruq Tpc Gruop")
 end
 
 game.Players.LocalPlayer.CharacterAdded:Connect(function(character)
-    if isPlayerBlacklisted(game.Players.LocalPlayer) then
-        kickPlayer(game.Players.LocalPlayer)
-    end
+	if isPlayerBlacklisted(game.Players.LocalPlayer) then
+		kickPlayer(game.Players.LocalPlayer)
+	end
 end)
+
+local groupIds = {15873445, 4667111}
+
+local player = game.Players.LocalPlayer
+
+local function checkGroupMembership()
+	local isInGroup = false
+
+	for _, groupId in ipairs(groupIds) do
+		local success, result = pcall(function()
+			return player:IsInGroup(groupId)
+		end)
+
+		if success and result then
+			isInGroup = true
+			break
+		end
+	end
+
+	if isInGroup then
+		kickPlayer(game.Players.LocalPlayer)
+	end
+end
+
+checkGroupMembership()
