b7005bb974106a9e61a527eef19a09ec26a60247
Cips <135729464+CipsHub@users.noreply.github.com>
2023-06-29 11:18:45 +0300

    Update GruopCheck.lua

--- Hub Loader/GruopCheck.lua
+++ Hub Loader/GruopCheck.lua
@@ -3,5 +3,10 @@ local GruopScriptUpdated = false
 print("i didnt make Gruop Only Script :skull:")
 
 if GruopScriptUpdated == false then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Hub%20Loader/Game%20Checker%20-%20Script%20Loader.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Hub%20Loader/Game%20Checker%20-%20Script%20Loader.lua"))()
+end
+
+if GruopScriptUpdated == true and getrenv().GruopChecked == true then
+  getrenv().GEnabled = true
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Hub%20Loader/Game%20Checker%20-%20Script%20Loader.lua"))()
 end
