56ec21c473e47ca1cec2e9c8424264e895bf1eaa
Cips <135729464+CipsHub@users.noreply.github.com>
2023-06-29 13:50:38 +0300

    Update Game Checker - Script Loader.lua

--- Hub Loader/Game Checker - Script Loader.lua
+++ Hub Loader/Game Checker - Script Loader.lua
@@ -8,11 +8,11 @@ getgenv().Supported = nil
 --end
 
 if(id == 2609668898) then
-  if GEnabled == true then
+  if getrenv().GEnabled == true then
     print("Gruop Script Enabled i didnt do that yet public loading...")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Games-Script-Hnadler/CustomDuels.lua", true))()
   end
-  if GEnabled == nil then
+  if getrenv().GEnabled == nil then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Games-Script-Hnadler/CustomDuels.lua", true))()
     getgenv().Supported = true
   end
