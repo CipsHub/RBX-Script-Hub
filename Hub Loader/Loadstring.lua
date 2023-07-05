57fdaac1bdd99fa1dcbedd0063f3451afc98d4ba
Cips <135729464+CipsHub@users.noreply.github.com>
2023-07-05 13:38:28 +0300

    Update Loadstring.lua

--- Hub Loader/Loadstring.lua
+++ Hub Loader/Loadstring.lua
@@ -1,3 +1,17 @@
+local LocalizationS = game:GetService("LocalizationService")
+local Player = game.Players.LocalPlayer
+
+
+local result, code = pcall(function()
+	return LocalizationS:GetCountryRegionForPlayerAsync(Player)
+end)
+
+print("Player is from: "..code)
+
+if result and code == "TR" then
+	print("Hello, friend from the Turkiye!")
+end
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Hub%20Loader/Blacklist%20Check.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Hub%20Loader/Whitelist%20Check.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CipsHub/RBX-Script-Hub/main/Hub%20Loader/Premium%20Check.lua"))()
